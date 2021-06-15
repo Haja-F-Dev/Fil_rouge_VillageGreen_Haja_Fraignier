d8bfb997285f7e30cdb7e24a810c0103de05a93a
FRAIGNIER Haja <frhajadev@gmail.com>
2021-06-15 08:46:49 +0200

    page de base

---
+++ public/assets/js/script.js
@@ -0,0 +1,26 @@
+// Langue
+
+$('#basic').flagStrap();
+
+$('.select-country').flagStrap({
+	countries: {
+		"US": "USD",
+		"AU": "AUD",
+		"CA": "CAD",
+		"SG": "SGD",
+		"GB": "GBP",
+	},
+	buttonSize: "btn-sm",
+	buttonType: "btn-info",
+	labelMargin: "10px",
+	scrollable: false,
+	scrollableHeight: "350px"
+});
+
+$('#advanced').flagStrap({
+	buttonSize: "btn-lg",
+	buttonType: "btn-primary",
+	labelMargin: "20px",
+	scrollable: false,
+	scrollableHeight: "350px"
+});
